9f2d004ac9f99ef2aa1cfd18aa9b488090009738
Mari Cruz Lopez Fernandez <maricruzlop@gmail.com>
2024-07-23 10:23:01 +0100

    Release 1 - Add a report + UX features (delete + edit)

---
+++ controllers/station-controller.js
@@ -0,0 +1,38 @@
+import { stationStore } from "../models/station-store.js";
+import { reportStore } from "../models/report-store.js";
+
+
+export const stationController = {
+  async index(request, response) {
+    const station = await stationStore.getStationById(request.params.id);
+    const viewData = {
+      title: "Station",
+      station: station,
+    };
+    response.render("station-view", viewData);
+  },
+
+  async addReport(request, response) {
+    const station = await stationStore.getStationById(request.params.id);
+    const newReport = {
+        code: Number(request.body.code),
+        temperature: Number(request.body.temperature),
+        windSpeed: Number(request.body.windSpeed),
+        windDirection:Number(request.body.windDirection),
+        pressure: Number(request.body.pressure),
+    };
+    console.log(`adding report ${newReport.code}`);
+    await reportStore.addReport(station._id, newReport);
+    response.redirect("/station/" + station._id);
+
+  },
+  
+
+  async deleteReport(request, response) {
+    const stationId = request.params.stationid;
+    const reportId = request.params.reportid;
+    console.log(`Deleting Report ${reportId} from Station ${stationId}`);
+    await reportStore.deleteReport(reportId);
+    await response.redirect("/station/" + stationId);
+  },
+};
--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
+import { stationController } from "./controllers/station-controller.js";
+import { reportController } from "./controllers/report-controller.js";
 
 
 export const router = express.Router();
@@ -10,5 +12,12 @@ router.get("/dashboard", dashboardController.index);
 router.get("/about", aboutController.index);
 
 router.post("/dashboard/addstation", dashboardController.addStation);
+router.get("/station/:id", stationController.index);
+router.post("/station/:id/addreport", stationController.addReport);
+router.get("/dashboard/deletestation/:id", dashboardController.deleteStation);
+router.get("/station/:stationid/deletereport/:reportid", stationController.deleteReport);
+
+router.get("/station/:stationid/editreport/:reportid", reportController.index);
+router.post("/station/:stationid/updatereport/:reportid", reportController.update);
 
 
